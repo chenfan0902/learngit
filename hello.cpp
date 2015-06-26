a1f2c4c48fee0caae949b1a373af8ea556a6f4ba
cyongquan <cyongquan@foxmail.com>
2015-06-26 21:26:44 +0800

    modify by chenyq

--- hello.cpp
+++ hello.cpp
@@ -5,5 +5,8 @@ int main() {
     //add some comment by chenyq
     cout<<"this is my first program!"<<endl;
 	cout<<"world world"<<endl;
+    if (true) {
+            return 1;
+    }
 	return 0;
 }
